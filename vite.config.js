36599965ee52c2646f1a9d2b85b7597f64458942
magallanesleo <leomagallanes43@gmail.com>
2024-11-23 12:08:36 +0800

    act 1.12

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+
+export default {
+    root: 'src/',
+    publicDir: '../static/',
+    base: './',
+    server:
+    {
+        host: true,
+        open: !isCodeSandbox // Open if it's not a CodeSandbox
+    },
+    build:
+    {
+        outDir: '../dist',
+        emptyOutDir: true,
+        sourcemap: true,
+        rollupOptions: {
+            input: {
+                index: './src/act12.html',
+            }
+        }
+    }
+}
